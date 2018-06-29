ac3eb9dcf5b72c4cf49d5bcf1e1678b2a3bcd2ff
Raymond Buckley <raymond.ray3k@gmail.com>
2018-06-28 23:26:02 -0700

    Initial commit

---
+++ desktop/src/com/ray3k/javapackager/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ray3k.javapackager.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ray3k.javapackager.Core;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Core(), config);
+	}
+}
